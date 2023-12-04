62d049cfbb1c00f917d5cee5858e41eca6833991
Neosoft <neosoft@Neosoft-Mac-M1.local>
2023-12-04 15:59:18 +0530

    initial commit

---
+++ lib/main_dev.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:shorebird_demo/flavors.dart';
+
+void main() async {
+  AppConfig.create(
+    appName: "Dev Flavor Example",
+    baseUrl: "https://flutterindiadev-dev.com",
+    primaryColor: Colors.red,
+    flavor: Flavor.dev,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: AppConfig.shared.primaryColor),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppConfig.shared.appName),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("App Name: ${AppConfig.shared.appName}"),
+            Text("Base URL: ${AppConfig.shared.baseUrl}"),
+            Text("Flavor: ${AppConfig.shared.flavor}"),
+            const Text('New text')
+          ],
+        ),
+      ),
+    );
+  }
+}
